25da5b7021897a6ecab4c5654d1e59ec180c3aa2
Cuni <cunigramajo@hotmail.com>
2022-05-18 05:12:44 -0300

    Editando scripts

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+console.log("Pedacho de nachi!");
 
 app.listen(4000, () => {
     console.log("Servidor corriendo en el puerto 4000.");
