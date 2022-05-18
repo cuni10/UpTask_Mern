b352f2cd312a8c22e6efd17824123241e6645658
Cuni <cunigramajo@hotmail.com>
2022-05-18 20:03:59 -0300

    Conectando mongodb y agregando variables de entorno.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import conectarDB from "./config/db.js";
 
 const app = express();
 
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+
+conectarDB();
+
 console.log("Pedacho de nachi!");
 
-app.listen(4000, () => {
-    console.log("Servidor corriendo en el puerto 4000.");
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}.`);
 });
