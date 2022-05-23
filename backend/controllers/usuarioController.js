8604237a130b5c265adeb2cc0851f8559bb9a8b1
Cuni <cunigramajo@hotmail.com>
2022-05-23 20:41:32 -0300

    agregando hash de passwords

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -4,7 +4,7 @@ const registrar = async (req,res)=>{
 
     const {email} = req.body;
 
-    const usuarioExiste = Usuario.findOne({email : email});
+    const usuarioExiste = await Usuario.findOne({email});
 
     if (usuarioExiste) {
 
@@ -12,16 +12,16 @@ const registrar = async (req,res)=>{
 
         return res.status(400).json({msg: error.message});
     }
+        try {
+            const usuario = new Usuario(req.body);
+            const usuarioAlmacenado = await usuario.save();
+    
+            res.json(usuarioAlmacenado);
+        } catch (error) {
+            console.log(error);
+        }
 
-    try {
-        const usuario = new Usuario(req.body);
-        const usuarioAlmacenado = await usuario.save();
-
-        res.json(usuarioAlmacenado);
-;
-    } catch (error) {
-        console.log(error);
-    }
+    
 };
 
 export {registrar};
--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+import bcrypt from "bcrypt";
+
 const usuarioSchema = mongoose.Schema({
     nombre:{
         type: String,
@@ -29,6 +31,17 @@ const usuarioSchema = mongoose.Schema({
 {
     timestamps:true,
 });
+
+usuarioSchema.pre("save", async function(next){
+
+    if(!this.isModified("password")){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+
+})
 
 const Usuario = mongoose.model("Usuario",usuarioSchema);
 
